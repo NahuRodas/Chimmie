7e494127ac1f88a2426d1673114d1a000b552631
NahuRodas <cesarnahuelrodas@gmail.com>
2023-02-07 14:41:39 -0300

    v2

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ document.querySelector('#app').innerHTML = `
               <h1>Lore</h1>
               <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
               <div class="lore-img">
-                <img src="./resources/chimmie-1.png" alt="Chimmie with rat">
+                <img src="https://raw.githubusercontent.com/NahuRodas/Chimmie/main/resources/chimmie-1.png" alt="Chimmie with rat">
               </div>
             </div>
             </div>
